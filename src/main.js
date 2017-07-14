70af37cec5d98b8ad1579e754c56e8a127558b14
zhufengnodejs <zhufengnodejs@126.com>
2017-07-14 19:13:39 +0800

    4.轮播图

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-import Home from './containers/Home';
-import Lesson from './containers/Lesson';
-import Profile from './containers/Profile';
-
-render(<Router>
-  <Switch>
-    <Route path='/home' component={Home}/>
-    <Route path='/profile' component={Profile}/>
-    <Route path='/Lesson' component={Lesson}/>
-    <Redirect to="/home"/>
-  </Switch>
-</Router>,document.querySelector('#root'));
+import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import Home from "./containers/Home/index";
+import Profile from "./containers/Profile/index";
+import Lesson from "./containers/Lesson/index";
+import App from "./containers/App";
+import './common/css/index.less';
+import store from './redux';//导出后的store
+window._store = store;
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
+import createHistory from 'history/createHashHistory';
+const history = createHistory();
+render(<Provider store={store}>
+  <ConnectedRouter history={history}>
+    <App>
+      <Switch>
+        <Route path={'/home'} component={Home}/>
+        <Route path={'/profile'} component={Profile}/>
+        <Route path={'/lesson'} component={Lesson}/>
+        <Redirect to="/home"/>
+      </Switch>
+    </App>
+  </ConnectedRouter>
+</Provider>, document.getElementById('app'));
