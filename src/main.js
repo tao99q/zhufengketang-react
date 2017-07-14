9f130485ee48b9e415d8bb1bedcd0d8731fbdc51
zhufengnodejs <zhufengnodejs@126.com>
2017-07-14 10:54:08 +0800

    2.跑通路由

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
-render(<h1>hello</h1>,document.querySelector('#root'));
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import Home from './containers/Home';
+import Lesson from './containers/Lesson';
+import Profile from './containers/Profile';
+
+render(<Router>
+  <Switch>
+    <Route path='/home' component={Home}/>
+    <Route path='/profile' component={Profile}/>
+    <Route path='/Lesson' component={Lesson}/>
+    <Redirect to="/home"/>
+  </Switch>
+</Router>,document.querySelector('#root'));
